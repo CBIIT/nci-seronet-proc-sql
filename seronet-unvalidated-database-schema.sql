6509e9f0ef70c70981bfb62f626512b4c29bf634
Purkayastha <anjan.purkayastha@nih.gov>
2020-12-09 08:51:17 -0500

    Corrected FOREIGN KEY syntax.

--- seronet-unvalidated-database-schema.sql
+++ seronet-unvalidated-database-schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE `Demographic_Data` (
-  `Research_Participant_ID` varchar(255) PRIMARY KEY,
+  `Research_Participant_ID` varchar(255) NOT NULL,
   `Age` int,
   `Race` varchar(255),
   `Ethnicity` varchar(255),
-  `Gender` varchar(255)
+  `Gender` varchar(255),
+  PRIMARY KEY (`Research_Participant_ID`),
+  FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Submission_MetaData` (`Research_Participant_ID`)
 );
 
 CREATE TABLE `Prior_Test_Result` (
-  `Research_Participant_ID` varchar(255) PRIMARY KEY,
+  `Research_Participant_ID` varchar(255) NOT NULL,
   `SARS_CoV_2_PCR_Test_Result` varchar(255),
   `SARS_CoV_2_PCR_Test_Result_Provenance` varchar(255),
   `Date_of_SARS_CoV_2_PCR_sample_collection` date,
@@ -65,20 +67,24 @@ CREATE TABLE `Prior_Test_Result` (
   `Seasonal_Coronavirus_Serology_Test_Result_Provenance` varchar(255),
   `Date_of_Seasonal_Coronavirus_Serology_Test` date,
   `Current_Seasonal_Coronavirus_infection` boolean,
-  `Duration_of_Seasonal_Coronavirus_infection` int
+  `Duration_of_Seasonal_Coronavirus_infection` int,
+  PRIMARY KEY (`Research_Participant_ID`),
+  FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`)
 );
 
 CREATE TABLE `Prior_Covid_Outcome` (
-  `Research_Participant_ID` varchar(255) PRIMARY KEY,
+  `Research_Participant_ID` varchar(255) NOT NULL,
   `Is_Symptomatic` boolean,
   `Date_of_Symptom_Onset` date,
   `Symptoms_Resolved` varchar(25),
   `Date_of_Symptom_Resolution` date,
-  `Covid_Disease_Severity` int
+  `Covid_Disease_Severity` int,
+  PRIMARY KEY (`Research_Participant_ID`),
+  FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`)
 );
 
 CREATE TABLE `Covid_Symptom` (
-  `Research_Participant_ID` varchar(255) PRIMARY KEY,
+  `Research_Participant_ID` varchar(255) NOT NULL,
   `Fever` boolean,
   `Temperature_over_100` boolean,
   `Chills` boolean,
@@ -97,11 +103,13 @@ CREATE TABLE `Covid_Symptom` (
   `Nausea_or_vomiting` boolean,
   `Abdominal_pain` boolean,
   `Diarrhea` boolean,
-  `Other_Symptom` boolean
+  `Other_Symptom` boolean,
+  PRIMARY KEY (`Research_Participant_ID`),
+  FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`)
 );
 
 CREATE TABLE `Comorbidity` (
-  `Research_Participant_ID` varchar(255) PRIMARY KEY,
+  `Research_Participant_ID` varchar(255) NOT NULL,
   `Disease_Severity` int,
   `Diabetes_Mellitus` boolean,
   `Hypertension` boolean,
@@ -113,11 +121,13 @@ CREATE TABLE `Comorbidity` (
   `Immunosuppressive_conditions` boolean,
   `Autoimmune_condition` boolean,
   `Inflammatory_Disease` boolean,
-  `Other_Comorbidity` varchar(255) COMMENT 'This should be a ICD-10 code.'
+  `Other_Comorbidity` varchar(255),
+  PRIMARY KEY (`Research_Participant_ID`),
+  FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`)
 );
 
 CREATE TABLE `Biospecimen` (
-  `Biospecimen_ID` varchar(255) PRIMARY KEY,
+  `Biospecimen_ID` varchar(255) NOT NULL,
   `Research_Participant_ID` varchar(255),
   `Test_Agreement` varchar(255),
   `Shipping_ID` varchar(255),
@@ -146,79 +156,96 @@ CREATE TABLE `Biospecimen` (
   `Viability_Hemocytometer_Count` int,
   `Live_Cells_Automated_Count` int,
   `Total_Cells_Automated_Count` int,
-  `Viability_Automated_Count` int
+  `Viability_Automated_Count` int,
+  PRIMARY KEY (`Biospecimen_ID`),
+  FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`)
 );
 
 CREATE TABLE `Collection_Tube` (
-  `Collection_Tube_Type_Lot_Number` varchar(255) PRIMARY KEY,
+  `Collection_Tube_Type_Lot_Number` varchar(255) NOT NULL,
   `Biospecimen_ID` varchar(255),
   `Collection_Tube_Type` varchar(255),
   `Collection_Tube_Type_Catalog_Number` varchar(255),
-  `Collection_Tube_Type_Expiration_Date` Date
+  `Collection_Tube_Type_Expiration_Date` date,
+  PRIMARY KEY (`Collection_Tube_Type_Lot_Number`, `Biospecimen_ID`),
+  FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Biospecimen` (`Biospecimen_ID`)
 );
 
 CREATE TABLE `Equipment` (
-  `Equipment_ID` varchar(255),
-  `Biospecimen_ID` varchar(255),
+  `Equipment_ID` varchar(255) NOT NULL,
+  `Biospecimen_ID` varchar(255) NOT NULL,
   `Equipment_Type` varchar(255),
-  `Equipment_Calibration_Due_Date` date
+  `Equipment_Calibration_Due_Date` date,
+  PRIMARY KEY (`Equipment_ID`, `Biospecimen_ID`),
+  FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Biospecimen` (`Biospecimen_ID`)
 );
 
 CREATE TABLE `Reagent` (
-  `Reagent_Lot_Number` varchar(255),
-  `Biospecimen_ID` varchar(255),
+  `Reagent_Lot_Number` varchar(255) NOT NULL,
+  `Biospecimen_ID` varchar(255) NOT NULL,
   `Reagent_Name` varchar(255),
   `Reagent_Catalog_Number` varchar(255),
-  `Reagent_Expiration_Date` date
+  `Reagent_Expiration_Date` date,
+  PRIMARY KEY (`Reagent_Lot_Number`, `Biospecimen_ID`),
+  FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Biospecimen` (`Biospecimen_ID`)
 );
 
 CREATE TABLE `Aliquot` (
-  `Aliquot_ID` varchar(255) PRIMARY KEY,
+  `Aliquot_ID` varchar(255) NOT NULL,
   `Biorepository_ID` varchar(255),
   `Biospecimen_ID` varchar(255),
   `Aliquot_Volume` int,
   `Aliquot_Units` varchar(255),
-  `Aliquot_Initials` varchar(3)
+  `Aliquot_Initials` varchar(3),
+  PRIMARY KEY (`Aliquot_ID`),
+  FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Biospecimen` (`Biospecimen_ID`)
 );
 
 CREATE TABLE `Aliquot_Tube` (
-  `Aliquot_Tube_Type_Lot_Number` varchar(255) PRIMARY KEY,
+  `Aliquot_Tube_Type_Lot_Number` varchar(255) NOT NULL,
   `Aliquot_Tube_Type` varchar(255),
   `Aliquot_Tube_Type_Catalog_Number` varchar(255),
   `Aliquot_Tube_Type_Expiration_Date` date,
-  `Aliquot_ID` varchar(255)
+  `Aliquot_ID` varchar(255) NOT NULL,
+  PRIMARY KEY (`Aliquot_Tube_Type_Lot_Number`, `Aliquot_ID`),
+  FOREIGN KEY (`Aliquot_ID`) REFERENCES `Aliquot` (`Aliquot_ID`)
 );
 
 CREATE TABLE `Biorepository_Aliquot` (
-  `Biorepository_Aliquot_BarCode` varchar(255) PRIMARY KEY,
+  `Biorepository_Aliquot_BarCode` varchar(255) NOT NULL,
   `Biorepository_ID` varchar(255),
   `Biorepository_Aliquot_Volume` int,
   `Biorepository_Aliquot_Volume_Units` varchar(255),
-  `Biorepository_Aliquot_Destination` varchar(255)
+  `Biorepository_Aliquot_Destination` varchar(255),
+  PRIMARY KEY (`Biorepository_Aliquot_BarCode`),
+  FOREIGN KEY (`Biorepository_ID`) REFERENCES `Aliquot` (`Biorepository_ID`)
 );
 
 CREATE TABLE `Confirmatory_Test_Result` (
-  `Research_Participant_ID` varchar(255),
-  `Assay_ID` varchar(255),
-  `Target_Antigen` varchar(255),
+  `Research_Participant_ID` varchar(255) NOT NULL,
+  `Assay_ID` varchar(255) NOT NULL,
+  `Target_Antigen` varchar(255) NOT NULL,
   `Assay_Kit_Lot_Number` varchar(255),
   `Assay_Result` varchar(255),
   `Assay_Replicate` int,
-  `Date_of_Test` Date,
+  `Date_of_Test` date,
   `Test_Operator_Initials` varchar(255),
-  PRIMARY KEY (`Research_Participant_ID`, `Assay_ID`, `Target_Antigen`)
+  PRIMARY KEY (`Research_Participant_ID`, `Assay_ID`, `Target_Antigen`),
+  FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`),
+  FOREIGN KEY (`Assay_ID`) REFERENCES `Assay_Target` (`Assay_ID`)
 );
 
 CREATE TABLE `Submission_MetaData` (
-  `Submission_ID` varchar(255) COMMENT 'This is the file id assigned by SeroNet to the Zipped File',
+  `Submission_ID` varchar(255) NOT NULL,
   `Submission_Date` Date,
   `Submission_time` timestamp,
   `Submission_CBC` varchar(255),
-  `Research_Participant_ID` varchar(255)
+  `Research_Participant_ID` varchar(255) NOT NULL,
+  PRIMARY KEY (`Submission_ID`)
 );
 
 CREATE TABLE `Assay_Metadata` (
-  `Assay_ID` varchar(255),
+  `Assay_ID` varchar(255) NOT NULL,
   `Technology_Type` varchar(255),
   `Assay_Name` varchar(255),
   `Assay_Manufacturer` varchar(255),
@@ -248,44 +275,15 @@ CREATE TABLE `Assay_Metadata` (
   `N_true_negative` int,
   `N_false_positive` int,
   `N_false_negative` int,
-  `Peformance_Statistics_Source` varchar(255)
+  `Peformance_Statistics_Source` varchar(255),
+  PRIMARY KEY (`Assay_ID`)
 );
 
 CREATE TABLE `Assay_Target` (
-  `Assay_ID` varchar(255),
-  `Target_Antigen` varchar(255),
+  `Assay_ID` varchar(255) NOT NULL,
+  `Target_Antigen` varchar(255) NOT NULL,
   `Target_Antigen_Sub_Region` varchar(255),
-  `Assay_Antigen_Source` varchar(255)
-  PRIMARY KEY (`Assay_ID`, `Target_Antigen`)
+  `Assay_Antigen_Source` varchar(255),
+  PRIMARY KEY (`Assay_ID`, `Target_Antigen`),
+  FOREIGN KEY (`Assay_ID`) REFERENCES `Assay_Metadata` (`Assay_ID`)
 );
-
-ALTER TABLE `Demographic_Data` ADD FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Submission_MetaData` (`Research_Participant_ID`);
-
-ALTER TABLE `Prior_Test_Result` ADD FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`);
-
-ALTER TABLE `Prior_Covid_Outcome` ADD FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`);
-
-ALTER TABLE `Covid_Symptom` ADD FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`);
-
-ALTER TABLE `Comorbidity` ADD FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`);
-
-ALTER TABLE `Biospecimen` ADD FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`);
-
-ALTER TABLE `Confirmatory_Test_Result` ADD FOREIGN KEY (`Research_Participant_ID`) REFERENCES `Demographic_Data` (`Research_Participant_ID`);
-
-ALTER TABLE `Aliquot` ADD FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Biospecimen` (`Biospecimen_ID`);
-
-ALTER TABLE `Equipment` ADD FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Biospecimen` (`Biospecimen_ID`);
-
-ALTER TABLE `Reagent` ADD FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Biospecimen` (`Biospecimen_ID`);
-
-ALTER TABLE `Biorepository_Aliquot` ADD FOREIGN KEY (`Biorepository_ID`) REFERENCES `Aliquot` (`Biorepository_ID`);
-
-ALTER TABLE `Assay_Target` ADD FOREIGN KEY (`Assay_ID`) REFERENCES `Confirmatory_Test_Result` (`Assay_ID`);
-
-ALTER TABLE `Assay_Target` ADD FOREIGN KEY (`Assay_ID`) REFERENCES `Assay_Metadata` (`Assay_ID`);
-
-ALTER TABLE `Biospecimen` ADD FOREIGN KEY (`Biospecimen_ID`) REFERENCES `Collection_Tube` (`Biospecimen_ID`);
-
-ALTER TABLE `Aliquot` ADD FOREIGN KEY (`Aliquot_ID`) REFERENCES `Aliquot_Tube` (`Aliquot_ID`);
-
